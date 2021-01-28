eef12ad563130ec60eba48c56e2900d11644f8d1
quoc khai <quockhai2110@gmail.com>
2021-01-28 18:00:29 +0700

    woocommerce

--- v32/setup.py
+++
@@ -1,8 +0,0 @@
-import os
-from cartmigration.models.setup import Setup
-file_config = 'cartmigration/etc/config.ini'
-if os.path.isfile(file_config):
-	print("File config is exist, setup db with file config")
-	print("----------------------------------")
-setup = Setup()
-setup.run()
